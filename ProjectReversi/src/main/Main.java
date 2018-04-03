3a807f09379ff2cfc81f892466302d51112d2b72
Timothy de Jongh <icetimux@gmail.com>
2018-04-03 13:01:56 +0200

    added Connection class

--- ProjectReversi/src/main/Main.java
+++ ProjectReversi/src/main/Main.java
@@ -3,6 +3,7 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		Connection connection = new Connection();
+        System.out.println(connection.startConnection("localhost", 7789));
 	}
 }

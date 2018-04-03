c866623ec6a712ffd693d546c4e960cfa9ccb18f
RvdM <rvdm1152@gmail.com>
2018-04-03 13:35:55 +0200

    Main Menu start

--- ProjectReversi/src/main/Main.java
+++ ProjectReversi/src/main/Main.java
@@ -1,9 +1,55 @@
 package main;
 
-public class Main {
-	public static char mode;
+import javafx.application.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+import javafx.event.*;
 
-	public static void main(String[] args) {
-		
+public class Main extends Application {
+	public static char mode;
+	public static String usernm;
+	
+	final TextField text = new TextField();
+	Label label = new Label("Username: ");
+	Button b1 = new Button("TicTacToe");
+	Button b2 = new Button("Reversi");
+	
+	@Override
+	public void start(Stage stage) {
+		try {
+			GridPane root = new GridPane();
+			root.setAlignment(Pos.CENTER);
+			root.add(label, 0, 0);
+			root.add(text, 1, 0);
+			root.add(b1, 0, 1);
+			root.add(b2, 1, 1);
+			
+			b1.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent ae) {
+					usernm = text.getText();
+				}
+			});
+			b2.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent ae) {
+					usernm = text.getText();
+				}
+			});
+			
+			Scene scene = new Scene(root, 400, 400);
+			
+			stage.setTitle("Test");
+			stage.setScene(scene);
+			stage.show();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }

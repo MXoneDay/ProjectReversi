74342e9a542283eb4aeb8a19c3a4262d2313691c
Masaru <rvdm1152@gmail.com>
2018-04-03 13:04:00 +0200

    Revert "Feature/setup server connection"

--- ProjectReversi/src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Connection connection = new Connection();
-        System.out.println(connection.startConnection("localhost", 7789));
-	}
-}

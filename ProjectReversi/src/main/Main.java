0fe59e293337e88ca38c7ee73d830ce35aa5631c
Timothy de Jongh <icetimux@gmail.com>
2018-04-04 12:43:53 +0200

    added receive serve messages functionality

--- ProjectReversi/src/main/Main.java
+++ ProjectReversi/src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Connection connection = new Connection();
-        System.out.println(connection.startConnection("localhost", 7789));
+        System.out.println(connection.start("localhost", 7789));
+		System.out.println("Started connection with server");
 	}
 }

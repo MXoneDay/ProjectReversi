0b673481916f66901abfb43d58d8aa67327a9e3d
RvdM <rvdm1152@gmail.com>
2018-04-30 10:55:25 +0200

    commit for marckck

--- ProjectReversi/src/model/Reversi.java
+++ ProjectReversi/src/model/Reversi.java
@@ -7,29 +7,6 @@ import view.CellPane;
 public class Reversi implements Game {
 	private final static int hor = 8, ver = 8;
 	
-	Reversi(){
-		/*
-		CellPane cp;
-		cp = board.getCell(4, 4);
-		cp.filled = 2;
-		cp.getChildren().add(getImage(2));
-		cp = board.getCell(4, 5);
-		cp.filled = 1;
-		cp.getChildren().add(getImage(1));
-		cp = board.getCell(5, 4);
-		cp.filled = 2;
-		cp.getChildren().add(getImage(2));
-		cp = board.getCell(5, 5);
-		cp.filled = 1;
-		cp.getChildren().add(getImage(1));*/
-		
-		/* TODO
-		split drawing and filling board.
-		drawing should happen in gamwFw every so often
-		filling in board can happen by all threads an at all times
-		 */
-	}
-	
 	@Override
 	public void setup(Board board) {
 		CellPane cp;
@@ -212,10 +189,89 @@ public class Reversi implements Game {
 
 	@Override
 	public boolean isValid(int turn, int hor, int ver, Board board) {
-		
-		if(move(turn, hor, ver, board, true)) {
-			return move(turn, hor, ver, board, false);
+		if(board.getCell(hor, ver).filled != 0) {
+			return false;
 		}
-		return false;
+		
+		boolean ret = false;;
+		boolean go = false;
+		int i = hor, j = ver, done = 0;
+		int enemy = (turn == 1) ? 2 : 1;
+		int cpFill;
+		CellPane cp;
+		
+		System.out.println("Move: " + hor + "-" + ver + " " + turn + " " + board.getCell(hor, ver).filled);
+		
+		while(done < 8) {
+			switch(done) {
+			case 0:
+				i++;
+				break;
+			case 1:
+				i--;
+				break;
+			case 2:
+				j++;
+				break;
+			case 3:
+				j--;
+				break;
+			case 4:
+				i++;
+				j++;
+				break;
+			case 5:
+				i--;
+				j--;
+				break;
+			case 6:
+				i--;
+				j++;
+				break;
+			case 7:
+				i++;
+				j--;
+				break;
+			}
+			try {
+				cpFill = board.getCell(i, j).filled;
+				
+				if(cpFill == enemy) {
+					go = true;
+				}else if(cpFill == 0 && !go) {
+					throw new Exception();
+				}else if(cpFill == turn) {
+					if(go) {
+						while(go) {
+							cp = board.getCell(i, j);
+							cp.getChildren().clear();
+							cp.filled = turn;
+							cp.getChildren().add(getImage(turn));
+							ret = true;
+							if(i == hor && j == ver) {
+								done++;
+								go = false;
+							}else {
+								if(i != hor) {
+									i = (i < hor) ? ++i : --i;
+								}
+								if(j != ver) {
+									j = (j < ver) ? ++j : --j;
+								}
+							}
+						}
+					}else {
+						throw new Exception();
+					}
+				}
+			}
+			catch(Exception ex) {
+				i = hor;
+				j = ver;
+				done++;
+				go = false;
+			}
+		}
+		return ret;
 	}
 }
